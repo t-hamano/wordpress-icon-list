823c32ca0b6912d86f17401cc8fe781c815dda39
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2022-08-29 14:28:29 +0000

    Polish copy action

--- src/components/icon-info.jsx
+++ src/components/icon-info.jsx
@@ -8,7 +8,13 @@ export default function IconInfo( { icon } ) {
 
 	return (
 		<Tooltip arrow open={ openTip } disableHoverListener placement="top" title={ 'Copied!!' }>
-			<CopyToClipboard text={ icon[ 0 ] } onCopy={ () => setOpenTip( true ) }>
+			<CopyToClipboard
+				text={ icon[ 0 ] }
+				onCopy={ () => {
+					setOpenTip( true );
+					setTimeout( () => setOpenTip( false ), 1000 );
+				} }
+			>
 				<button type="button" onMouseLeave={ () => setOpenTip( false ) }>
 					<Icon icon={ icon[ 1 ] } viewBox="0 0 24 24" />
 					<span>{ icon[ 0 ] }</span>
